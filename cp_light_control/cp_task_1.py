78372aa4c67d2dc9301086c312c41853a9b63e64
Vannak <94319566+Vlak23@users.noreply.github.com>
2022-02-11 09:57:00 +0700

    Update cp_task_1.py

--- cp_light_control/cp_task_1.py
+++ cp_light_control/cp_task_1.py
@@ -2,10 +2,8 @@ import time
 import board
 Analog_in = AnalogIn(board.A1) 
 
-def get_voltage(pin) : 
-   return (pin.value * 3.3) / 65536
 
 while True:
-    print((get_voltage(analog_in),))
-    time.sleep(0.1)
+    print(analog_in.value)
+    time.sleep(0.5)
 
